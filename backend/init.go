7a32750daeb8ebd725b6b25b6d33ca0b0b172c98
Koston Zhuang <koston.zhuang@maiscrm.com>
2024-03-06 10:53:32 +0800

    backend: init permissions

--- backend/init.go
+++ backend/init.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	model_common "pchat/model/common"
+	model_user "pchat/model/user"
 )
 
 func InitDefaultResources() {
 	ctx := context.Background()
 	model_common.CSetting.CreateDefaultSetting(ctx)
+	model_user.CPermission.Init(ctx)
 }
--- backend/model/user/permission.go
+++ backend/model/user/permission.go
@@ -1,6 +1,11 @@
 package user
 
-import "pchat/repository/bson"
+import (
+	"context"
+	"pchat/permissions"
+	"pchat/repository"
+	"pchat/repository/bson"
+)
 
 const (
 	C_PERMISSION = "permission"
@@ -13,4 +18,21 @@ var (
 type Permission struct {
 	Id   bson.ObjectId `bson:"_id"`
 	Name string        `bson:"name"`
+}
+
+func (*Permission) Init(ctx context.Context) error {
+	for _, permission := range permissions.AllPermissions {
+		condition := bson.M{
+			"name": permission,
+		}
+		updater := bson.M{
+			"$set": bson.M{
+				"name": permission,
+			},
+		}
+		if err := repository.Upsert(ctx, C_PERMISSION, condition, updater); err != nil {
+			return err
+		}
+	}
+	return nil
 }
